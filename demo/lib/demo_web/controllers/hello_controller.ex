e9465270c8a8ebba4b4b1d5c1a14ade79ec2bab2
Fernando Daciuk <f.daciuk@gmail.com>
2021-11-23 08:00:10 -0300

    add show view for hello route

--- demo/lib/demo_web/controllers/hello_controller.ex
+++ demo/lib/demo_web/controllers/hello_controller.ex
@@ -4,4 +4,9 @@ defmodule DemoWeb.HelloController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def show(conn, %{"messenger" => messenger}) do
+    conn
+    |> render("show.html", messenger: messenger)
+  end
 end
--- demo/lib/demo_web/router.ex
+++ demo/lib/demo_web/router.ex
@@ -19,6 +19,7 @@ defmodule DemoWeb.Router do
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
   end
 
   scope "/api", DemoWeb do
